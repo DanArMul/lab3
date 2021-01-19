248b74a1e536eb1c91efe9096702e0693eae9f1b
Daniel Muljono <dmuljono@ucsd.edu>
2021-01-19 16:35:04 +0700

    changes made

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,9 +10,27 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("Javascript has taken control");
+		$('.jumbotron p').toggleClass("active");
+		$("#testjs").text("Please wait...");
 	});
-
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+	$("a.thumbnail").click(projectClick);
 }
+
+
+
+function projectClick(){
+	    var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+       description.fadeToggle();
+    }
+}
+
+
+
+
